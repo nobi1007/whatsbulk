f7955c2ee27c72c4b91402f685c65d1870a5d50f
nobi1007 <mittalshyam1007@gmail.com>
2020-11-10 17:14:41 +0530

    update scripts

--- setup-mac.sh
+++ setup-mac.sh
@@ -5,9 +5,9 @@ brew install python3
 
 echo -e '\n ---- Creating virtualenv and activating it ---- \n'
 virtualenv whatsbulk-venv 
-source whatsbulk-venv/bin/activate
+source ./whatsbulk-venv/bin/activate
 
 echo -e '\n ---- Installing dependencies ----\n'
-pip install -r requirements.txt
+pip install -r ./requirements.txt
 
 echo -e '\n ---- Success, now you can run start.sh ----\n'
